9327eb2210aebf4b2c9868865c183f57428fcd3b
Vincent Brebion <vbrebion@sodius.com>
2020-09-07 15:39:54 +0200

    Fireworks incoming

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,40 +2,20 @@ import org.openrndr.application
 import org.openrndr.color.ColorHSVa
 import org.openrndr.draw.Drawer
 import org.openrndr.math.Vector2
-import kotlin.math.*
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.random.Random
 
-class Firework(width: Int, height: Int, seconds: Double) {
-    var pos = Vector2(width / 2.0 + Random.nextDouble(-width / 5.0, width / 5.0), height.toDouble())
-    var vel = Vector2(Random.nextDouble(-.5, .5), -Random.nextDouble(8.0, 10.0))
-    var fuse = seconds + Random.nextDouble(30.0, 40.0)
-    var color = ColorHSVa(Random.nextDouble(.0, 360.1), .5, 1.0)
-    var explode = true
-
-    constructor(
-        width: Int,
-        height: Int,
-        seconds: Double,
-        pos: Vector2,
-        vel: Vector2,
-        fuse: Double,
-        color: ColorHSVa
-    ) : this(width, height, seconds) {
-        this.pos = pos
-        this.vel = vel
-        this.fuse = fuse
-        this.color = color
-        this.explode = false
-    }
-
-    fun update(drawer: Drawer, seconds: Double) {
-        if (this.fuse > .0) {
+class Firework(var pos: Vector2, var vel: Vector2, var fuse: Double, var color: ColorHSVa, val explode: Boolean = false) {
+    fun update(drawer: Drawer, time: Double) {
+        if (fuse > .0) {
             drawer.stroke = this.color.toRGBa()
 
             drawer.circle(this.pos, 2.0)
 
-            this.pos += this.vel
-            this.fuse -= seconds
+            pos += this.vel
+            fuse -= time
         }
     }
 }
@@ -47,32 +27,48 @@ fun main() = application {
     }
 
     program {
-        val fireworks = MutableList(1) { Firework(width, height, seconds) }
-
-        mouse.buttonDown.listen { fireworks.add(Firework(width, height, seconds)) }
+        val fireworks = mutableListOf<Firework>()
+        val delay = 1.0
+        var time = .0
 
         extend {
-            (0 until fireworks.size).forEach {
-                val f = fireworks[it]
-                f.update(drawer, seconds)
+            time += seconds
 
-                if (f.fuse < .0 && f.explode) {
-                    (0..20).forEach { frag ->
-                        fireworks.add(
-                            Firework(
-                                width,
-                                height,
-                                seconds,
-                                f.pos,
-                                Vector2(asin(Random.nextDouble()), acos(Random.nextDouble())),
-                                seconds + 100.0,
-                                f.color
-                            )
+            if (time > delay) {
+                time -= delay
+
+                fireworks.add(
+                        Firework(
+                                Vector2(width / 2.0, height.toDouble()),
+                                Vector2(Random.nextDouble(-10.0, 10.0), -Random.nextDouble(10.0, 15.0)),
+                                Random.nextDouble(.6, .8),
+                                ColorHSVa(Random.nextDouble(.0, 360.1), .5, 1.0),
+                                true
                         )
+                )
+
+                (0 until fireworks.size).forEach {
+                    val f = fireworks[it]
+                    f.update(drawer, deltaTime)
+
+                    if (f.fuse < .0 && f.explode) {
+                        val theta = 2.0 * PI / 50
+
+                        (0..50).forEach { frag ->
+                            fireworks.add(
+                                    Firework(
+                                            f.pos,
+                                            Vector2(cos(theta * frag), sin(theta * frag)) * 2.0,
+                                            Random.nextDouble(.4, .6),
+                                            f.color
+                                    )
+                            )
+                        }
                     }
                 }
             }
-            fireworks.removeIf { it.fuse < .0 }
+
+            fireworks.removeIf { it.fuse <= .0 }
         }
     }
 }
